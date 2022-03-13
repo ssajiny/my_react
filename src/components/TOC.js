e4be4b02ce31bf68c49e2c38396c6626bc9d7896
ssajiny <ssajiny@naver.com>
2022-03-13 16:57:01 +0900

    second

--- src/components/TOC.js
+++ src/components/TOC.js
@@ -5,12 +5,22 @@ class TOC extends Component {
         var lists = [];
         var data = this.props.data;
         var i = 0;
-        while(i < data.length){
-            lists.push(<li><a href={"/content/"+data[i].id}>{data[i].title}</a></li>)
-            i = i+1;
+        while (i < data.length) {
+            lists.push(
+                <li key={data[i].id}>
+                    <a 
+                        href={"/content/" + data[i].id}
+                        data-id={data[i].id}
+                        onClick={function(id, e){
+                            e.preventDefault();
+                            this.props.onChangePage(id);
+                        }.bind(this, data[i].id)}
+                    >{data[i].title}
+                    </a>
+                </li>
+            );
+            i = i + 1;
         }
-
-
         return (
             <nav>
                 <ul>
